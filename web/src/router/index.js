be202838269b152723926750bbdd0932ca979069
greennull <285399141@qq.com>
2019-11-11 16:01:00 +0800

    提交路由

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -53,11 +53,56 @@ export const constantRoutes = [
       }
     ]
   },
+  //shop路由
   {
     path: '/login',
     component: () => import('@/views/login/index'),
     hidden: true
   },
+  {
+    path: '/',
+    component: () => import('@/views/shop/index'),
+    redirect: '/index',
+    children: [
+      {
+        path: '/home',
+        component: () => import('@/views/shop/HomePage/Home'),
+        name: 'home',
+        meta: { title: '商城首页', icon: 'dashboard', affix: true }
+      },
+      {
+        path: '/order',
+        component: () => import('@/views/shop/OrderPage/Order'),
+        name: 'order',
+        meta: { title: '我的订单', icon: 'dashboard', affix: true }
+      },
+      {
+        path: '/loginIn',
+        component: () => import('@/views/shop/LoginPage/LoginIn'),
+        name: 'loginIn',
+        meta: { title: '登录/注册', icon: 'dashboard', affix: true }
+      },
+      {
+        path: '/charts',
+        component: () => import('@/views/shop/ChartsPage/Charts'),
+        name: 'charts',
+        meta: { title: '图表', icon: 'dashboard', affix: true }
+      },
+      {
+        path: '/prodetails',
+        component: () => import('@/views/shop/DetailPage/ProDetails'),
+        name: 'prodetails',
+        meta: { title: '商品详情', icon: 'dashboard', affix: true }
+      },
+      {
+        path: '/mycarts',
+        component: () => import('@/views/shop/CartsPage/Carts'),
+        name: 'mycarts',
+        meta: { title: '我的购物车', icon: 'dashboard', affix: true }
+      }
+    ]
+  },
+
   {
     path: '/auth-redirect',
     component: () => import('@/views/login/auth-redirect'),
@@ -72,19 +117,6 @@ export const constantRoutes = [
     path: '/401',
     component: () => import('@/views/error-page/401'),
     hidden: true
-  },
-  {
-    path: '/',
-    component: Layout,
-    redirect: '/dashboard',
-    children: [
-      {
-        path: 'dashboard',
-        component: () => import('@/views/dashboard/index'),
-        name: 'Dashboard',
-        meta: { title: 'Dashboard', icon: 'dashboard', affix: true }
-      }
-    ]
   },
   {
     path: '/documentation',
