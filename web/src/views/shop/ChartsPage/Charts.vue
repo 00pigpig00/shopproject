acedde248fd611b75941213c1cb665080e569074
00pigpig00 <937144431@qq.com>
2019-11-13 11:56:17 +0800

    charts样式添加(无mock模拟)

--- web/src/views/shop/ChartsPage/Charts.vue
+++ web/src/views/shop/ChartsPage/Charts.vue
@@ -1,84 +1,92 @@
 <template>
 
-       
-    <div id="">
-      <div id="charts">
+     
+  <div id="">
+    <div id="charts">
 
-      </div>
     </div>
+  </div>
 
 </template>
 
 
 <script>
-import echarts from 'echarts';
-import axios from 'axios';
-import {fetchList} from '@/api/shop'
+  import echarts from 'echarts';
+  import axios from 'axios';
+  import {fetchList} from '@/api/shop'
 
-    export default {
-        name: 'charts',
-        data() {
+  export default {
+    name: 'charts',
+    data() {
 
-            return {msg: '图标',
-              list:[]
-            }
+      return {
+        msg: '图标',
+        list: []
+      }
 
-        },
-        mounted() {
+    },
+    mounted() {
 
 
-          fetchList().then(res=>{
-            console.log(res.data);
-            this.list=res.data.items;
-            this.init();
+      fetchList().then(res => {
+        console.log(res.data);
+        this.list = res.data.items;
+        this.init();
 //            this.total=res.data.items.length;
-          })
-        },
-      methods: {
-        init(){
-          let that =this
-          this.chart = echarts.init(document.getElementById("charts"));
-          this.chart.setOption({
-            legend: {},
-            tooltip: {},
-            dataset: {
-              source: [
-                ...this.list
-//                that.list[0],that.list[1],that.list[2],that.list[3]
-//                ['product', '2016', '2017', '2018', '2019'],
-//                ['手机卡', 41.1, 30.4, 65.1, 53.3],
-//                ['宽带', 86.5, 92.1, 85.7, 83.1],
-//                ['手机', 24.1, 67.2, 79.5, 86.4]
-              ]
-            },
-            xAxis: {type: 'category'},
-            yAxis: {axisLabel:{
-              formatter:'{value} %'
-            }
-            },
-
-
-//            grid: [
-//              {bottom: '55%'},
-//              {top: '55%'}
-//            ],
-            series: [
-              {type: 'bar'},
-              {type: 'bar'},
-              {type: 'bar'},
-              {type: 'bar'}
+      })
+    },
+    methods: {
+      init() {
+        let that = this
+        this.chart = echarts.init(document.getElementById("charts"));
+        this.chart.setOption({
+          legend: {},
+          tooltip: {},
+          dataset: {
+            source: [
+              ...this.list
             ]
-          })
-        }
+          },
+          xAxis: [
+            {type: 'category', gridIndex: 0},
+            {type: 'category', gridIndex: 1}
+          ],
+          yAxis: [{
+            gridIndex: 0,
+            axisLabel: {
+              formatter: '{value} %'
+            }
+          },
+            {gridIndex: 1,}
+          ],
+
+
+          grid: [
+            {bottom: '55%'},
+            {top: '55%'}
+          ],
+          series: [
+            {type: 'bar'},
+            {type: 'bar'},
+            {type: 'bar'},
+            {type: 'bar'},
+
+            {type: 'bar', xAxisIndex: 1, yAxisIndex: 1},
+            {type: 'bar', xAxisIndex: 1, yAxisIndex: 1},
+            {type: 'bar', xAxisIndex: 1, yAxisIndex: 1},
+            {type: 'bar', xAxisIndex: 1, yAxisIndex: 1}
+          ]
+        })
+      }
     }
 
-    }
+  }
 
 </script>
 
 
 <style>
-  #charts{
+  #charts {
     position: relative;
     width: 100%;
     height: calc(100vh - 84px);
