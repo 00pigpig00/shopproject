03c49d1b7e9a793bdbd1ac599336eeb932337bad
00pigpig00 <937144431@qq.com>
2019-11-19 14:56:14 +0800

    图表增加地区数据chartsage

--- web/src/views/shop/ChartsPage/ChartsAge.vue
+++ web/src/views/shop/ChartsPage/ChartsAge.vue
@@ -56,7 +56,7 @@
               name: '访问来源',
               type: 'pie',
               radius : '35%',
-              center: ['40%', '50%'],
+              center: ['50%', '60%'],
               data:[
                 {value:332139, name:'1960前'},
                 {value:75323, name:'1960-1970'},
@@ -103,7 +103,7 @@
               name: '访问来源',
               type: 'pie',
               radius : '35%',
-              center: ['40%', '50%'],
+              center: ['50%', '60%'],
               data:[
                 {value:2737, name:'1960前'},
                 {value:7873, name:'1960-1970'},
@@ -150,7 +150,7 @@
               name: '访问来源',
               type: 'pie',
               radius : '35%',
-              center: ['40%', '50%'],
+              center: ['50%', '60%'],
               data:[
                 {value:776, name:'1960前'},
                 {value:3462, name:'1960-1970'},
